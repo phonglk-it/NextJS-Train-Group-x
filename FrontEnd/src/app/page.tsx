70f2bd8ec1d03159490849ba0dda5d57d9bb1f86
NguyenHuuDay1808 <Daynh.developer@gmail.com>
2025-06-20 13:43:01 +0700

    aaa

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -1,8 +1,24 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Home Page</h1>
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import CustomersPage from "../components/Customers/page";
+import StylePage from "../components/Style/page";
+import Header from "../components/Header/header";
+import Footer from "../components/Footer/footer";
+import HomePage from "./(main)/homepage/page";
+import ProductsPage from "../components/Products/page";
 
-    </div>
+export default function Home() {
+  const [showAnnouncement, setShowAnnouncement] = useState(true);
+
+  return (
+    <main>
+      <Header />
+      <HomePage />
+      <StylePage />
+      <ProductsPage />
+      <Footer />
+      <Footer />
+    </main>
   );
 }
