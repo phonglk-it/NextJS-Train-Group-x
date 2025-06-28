97d71dcbdd6e95177ddc1133b3d69801b7834acc
minhnqgcc210356 <145000322+minhnqgcc210356@users.noreply.github.com>
2025-06-28 17:02:51 +0700

    add product id

--- FrontEnd/src/components/Customers/customers.tsx
+++ FrontEnd/src/components/Customers/customers.tsx
@@ -1,15 +1,15 @@
-'use client'
-import React, { useEffect } from 'react';
-import styles from './customers.module.css';
-import clsx from 'clsx';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
+"use client";
+import React, { useEffect } from "react";
+import styles from "./customers.module.css";
+import clsx from "clsx";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Customers = () => {
   useEffect(() => {
@@ -45,40 +45,49 @@ const Customers = () => {
           breakpoints={{
             0: { slidesPerView: 1 },
             768: { slidesPerView: 2 },
-            1024: { slidesPerView: 3 }
+            1024: { slidesPerView: 3 },
           }}
         >
           {[
-        {
-            name: 'Sarah M.',
-            comment: "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations."
-        },
-        {
-            name: 'Alex K.',
-            comment: "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions."
-        },
-        {
-            name: 'James L.',
-            comment: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends."
-        },
-        {
-            name: 'Sarah M.',
-            comment: "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations."
-        },
-        {
-            name: 'Alex K.',
-            comment: "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions."
-        },
-        {
-            name: 'James L.',
-            comment: "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends."
-        }
-        ]
-        .map((customer, index) => {
+            {
+              name: "Sarah M.",
+              comment:
+                "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+            },
+            {
+              name: "Alex K.",
+              comment:
+                "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.",
+            },
+            {
+              name: "James L.",
+              comment:
+                "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+            },
+            {
+              name: "Sarah M.",
+              comment:
+                "I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.",
+            },
+            {
+              name: "Alex K.",
+              comment:
+                "Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.",
+            },
+            {
+              name: "James L.",
+              comment:
+                "As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.",
+            },
+          ].map((customer, index) => {
             return (
               <SwiperSlide key={index}>
                 <div className={styles.item}>
-                  <img className={styles.stars} src="/images/Stars.png" alt="Stars icon" />
+                  <img
+                    className={styles.stars}
+                    src="/images/Stars.png"
+                    alt="Stars icon"
+                  />
                   <div className={styles.customer}>
                     <p className={styles.customerName}>{customer.name}</p>
                     <img src="/images/Stick.png" alt="Stick icon" />
