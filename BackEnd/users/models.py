d32fa7ae786fa5f909c371d24cd0b737e2fdf174
phonglk-it <phonglk.it@gmail.com>
2025-07-04 01:28:11 +0700

    Create model Cart

--- BackEnd/users/models.py
+++ BackEnd/users/models.py
@@ -1,8 +1,27 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+from products.models import Product
+from django.contrib.auth.models import AbstractUser
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='carts')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Cart {self.id} - {self.user.username}"
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
+    
 
 class CustomUser(AbstractUser):
-    phone = models.CharField(max_length=15, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.username
