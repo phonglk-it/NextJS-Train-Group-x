a1e39b784f40072a605702aa903b0ba1612153aa
NguyenHuuDay1808 <Daynh.developer@gmail.com>
2025-07-03 23:59:07 +0700

    Correct api route of categorycategory

--- BackEnd/core/urls.py
+++ BackEnd/core/urls.py
@@ -25,7 +25,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('product/', include('products.urls')),
-    path('api/', include('category.urls')),
+    path('categories/', include('category.urls')),
     
     path('schema/', spectacular_views.SpectacularAPIView.as_view(), name="schema"),
     path('swagger/', spectacular_views.SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
